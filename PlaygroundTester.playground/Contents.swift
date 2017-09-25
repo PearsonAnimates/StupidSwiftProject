5a07deb1c17d9ae12948c460c67c409413411f94
Yael_Amaya <yiap2610@gmail.com>
2017-09-25 13:51:48 -0600

    Added Stupid variables

--- PlaygroundTester.playground/Contents.swift
+++ PlaygroundTester.playground/Contents.swift
@@ -5,3 +5,15 @@ import UIKit
 var myStupid :String
 var otherStupid = "Stupid"
 var thisStupid :String = "Next Stupid"
+
+let CalvinNumber = 72.0
+let ParkerNumber = 666.0
+let MakaiNumber = 70.0
+let ParkerCalvinNumber = (ParkerNumber)+(CalvinNumber)
+let ParkerCalvinDividedByMakaiNumber = (ParkerCalvinNumber)/(MakaiNumber)
+var myMessage = "Parker and Calvin combined are \(ParkerCalvinNumber), because Parker is \(ParkerNumber), and Calvin is \(CalvinNumber)"
+var myMessage2 = "Now, the Makai number is \(MakaiNumber), so Parker and Calvin divided by Makai is \(ParkerCalvinDividedByMakaiNumber), corrupting \(CalvinNumber) with the devil and random number!"
+
+var ParkerAge = 89654
+var ParkerSpeed = 85.456
+var rate = ParkerAge/Int (ParkerSpeed)
