3428a4e81387ee6832d557cc474982463899668a
Yael_Amaya <yiap2610@gmail.com>
2017-09-27 11:54:32 -0600

    added playground for method testing

---
+++ Fisher.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+public func isReallySimple() -> Void
+{
+    print("It will be warm this weekend!!! ... cry.")
+}
+isReallySimple()
+
+public func aBitLessSimple(name: String ) -> Void
+{
+    let answer = "My name is not " + name
+    print(answer)
+}
+aBitLessSimple(name: "Porker Brailow")
+let words = "Parker"
+aBitLessSimple(name: words)
+
