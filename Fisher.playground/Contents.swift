b9c02fff14b7dbfd61ff4c2023cc8767767a3b5f
Yael_Amaya <yiap2610@gmail.com>
2017-09-27 12:14:17 -0600

    testing a counter

--- Fisher.playground/Contents.swift
+++ Fisher.playground/Contents.swift
@@ -10,10 +10,41 @@ isReallySimple()
 
 public func aBitLessSimple(name: String ) -> Void
 {
-    let answer = "My name is not " + name
+    let answer = "His name is not " + name
     print(answer)
 }
 aBitLessSimple(name: "Porker Brailow")
-let words = "Parker"
+let words = "Marker"
 aBitLessSimple(name: words)
+
+public func namingExample(outerName innerName :String) -> Void
+{
+    print("His actual name might be: \(innerName)")
+}
+namingExample(outerName: "Parker Brailow")
+
+public func counting() -> Int
+{
+    return (7 * 655)
+}
+print("He is not \(counting()) years old yet!")
+
+if(counting() < 10)
+{
+    print("Math works in swift too!!!")
+}
+else
+{
+    print("not likely")
+}
+var test = 0
+while (test < counting())
+{
+    print("hahaha")
+    test += 1
+}
+print("yeh!")
+
+
+
 
