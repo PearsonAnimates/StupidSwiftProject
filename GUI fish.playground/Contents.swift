f7da520560e040cf56e7320e4e2bf04b685a79d1
Yael_Amaya <yiap2610@gmail.com>
2017-10-12 13:20:14 -0600

    added timer, new screen with own controller, and a epilepsy button
    don't click epilepsy button, it crashes your system, working on it

---
+++ GUI fish.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "OMG I HAVE A GUI FISH!!!!"
+
+var myFirstButton = UIButton(frame: CGRect(x:30, y:60, width: 600, height: 120))
+myFirstButton.setTitle("Something better than Calvin", for: .normal)
+myFirstButton.backgroundColor = .gray
+myFirstButton.setTitleColor(.black, for: .normal)
+myFirstButton.titleLabel!.font = UIFont(name: "AmericanTypewriter-Bold", size: 40)
+myFirstButton
+
